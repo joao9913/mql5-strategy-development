272aca1b3eb1bafc9b376bff70a09f8335a3ab91
João Pereira <joaopereira9913@gmail.com>
2025-11-11 20:26:21 +0000

    Finished first version of joinsim ulations

--- Scripts/JoinSimulations.py
+++ Scripts/JoinSimulations.py
@@ -11,6 +11,12 @@ commonFolder = Path(os.getenv("APPDATA")) / "MetaQuotes" / "Terminal" / "Common"
 if not commonFolder.exists():
     raise FileNotFoundError(f"Folder not found: {commonFolder}")
 
+#Check if there are any folders to merge
+subfolders = [f for f in commonFolder.iterdir() if f.is_dir()]
+if not subfolders:
+    print(f"No subfolders found in {commonFolder}. No simulations to merge.")
+    exit()
+
 # ==========================
 # LOAD & MERGE CSVs
 # ==========================
@@ -28,10 +34,16 @@ for subfolder in commonFolder.iterdir():
             strategyName = subfolder.name.split("_")[0]
             df["Strategy"] = strategyName
 
+            cols = ["Strategy"] + [c for c in df.columns if c != "Strategy"]
+            df = df[cols]
+
             mergedDF = pd.concat([mergedDF, df], ignore_index=True)
         else:
             print(f"PHASE1.CSV not found in {subfolder}")
     
-outputFile = commonFolder / "PHASE1_MERGED.csv"
-mergedDF.to_csv(outputFile, index=False)
+outputFile = commonFolder / "PHASE1.csv"
+mergedDF["End Phase Date"] = pd.to_datetime(mergedDF["End Phase Date"], errors="coerce")
+mergedDF = mergedDF.sort_values("End Phase Date").reset_index(drop=True)
+mergedDF["End Phase Date"] = mergedDF["End Phase Date"].dt.strftime("%Y.%m.%d")
+mergedDF.to_csv(outputFile, index=False, encoding='utf-16', sep='\t')
 print(f"Merged simulations saved to: {outputFile}")
